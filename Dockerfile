618da5442402962346cdfed3834fe5367d7c05d6
Ashrafalaghbari <ashrafalaghbari@hotmail.com>
2023-04-21 11:47:57 +0800

    Add a streamlit web app

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,36 @@
+# # Base image
+# FROM python:3.10.7
+
+# # Copy files
+# COPY . /FBHP_app
+
+# # Set working directory
+# WORKDIR /FBHP_app
+
+# # Install dependencies
+# RUN pip install -r requirements.txt
+
+# # Expose port
+# EXPOSE 8080
+
+# # # Start Streamlit app
+# ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
+
+# Base image
 FROM python:3.10.7
+
+# Copy files
 COPY . /FBHP_app
+
+# Set working directory
 WORKDIR /FBHP_app
+
+# Install dependencies
 RUN pip install -r requirements.txt
-EXPOSE $PORT
-CMD gunicorn --workers=4 --bind 0.0.0.0:$PORT app:app
+
+# Start Streamlit app
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=5000", "--server.address=0.0.0.0"]
+
+
+
+ 
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, render_template
-import pickle
+import streamlit as st
+import pandas as pd
 import numpy as np
+import pickle
+import io
+import xlsxwriter
+import datetime
 
-app = Flask(__name__)
 
 # Load the trained model and scaler
 with open('lr_model.pickle', 'rb') as f:
@@ -10,57 +13,125 @@ with open('lr_model.pickle', 'rb') as f:
 with open('scaler_5.pickle', 'rb') as f:
     scaler = pickle.load(f)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict_by_values():
-    """
-    Predicts the output based on the form data received from a POST request.
-    Expects the form data to contain a dictionary with keys "oil_rate", "depth",
-    "water_rate", "tubing_id_1_9992", and "pwh". The values of "oil_rate",
-    "depth", "water_rate", and "pwh" are expected to be floating point numbers.
-    The value of "tubing_id_1_9992" is expected to be either "Yes" or "No".
-    Returns the predicted output as a string rendered by the "home.html" template.
-    """
-    data = request.form.to_dict()
-    for key, value in data.items():
-        if key != "tubing_id_1_9992":
-            data[key] = float(value)
-  
-
-    # Convert the categorical feature 'tubing_id_1_9992' to binary format
-    tubing_id_1_9992_binary = 1 if data['tubing_id_1_9992'] == 'Yes' else 0
-
-    # Centering and squaring the input oil rate
-    oil_train_mean = 6195.78527607362 # Mean of oil rate in the training set
-    oil_c_squared = (data['oil_rate'] - oil_train_mean)**2
+def preprocess(scaler, data):
+    tubing_id_1_995_binary = 1 if data['tubing_id_1_995'] == 'Yes' else 0
+    mean_oil_train = 6195.78527607362 # Mean of Oil_rate in the training set
+    oil_c_squared = (data['oil_rate'] - mean_oil_train)**2
+    input_arr = [oil_c_squared, data["depth"], data["water_rate"], tubing_id_1_995_binary, data["pwh"]]
+    scaled_input = scaler.transform([input_arr])
+    constant = [1]
+    input_with_constant = constant + list(scaled_input[0])
+    
+    return input_with_constant
 
 
-    # Create a numpy array with the input values (excluding the categorical feature)
-    input_arr = np.array([oil_c_squared, 
-                          data['depth'], 
-                          data['water_rate'], 
-                          tubing_id_1_9992_binary, 
-                          data['pwh']])
-   
+def predict_online(model, scaler, data):
+    preprocessed_data = preprocess(scaler, data)
+    prediction_output = model.predict([preprocessed_data])[0]
+    return prediction_output
 
-    # Reshape the array to have one row and the same number of columns as the training data
-    input_arr = input_arr.reshape(1, -1)
+def predict_batch(model, scaler, data):
 
-    # # Scale the input using the loaded scaler
-    scaled_input = scaler.transform(input_arr)
+    # Perform prediction on the input data
+    predictions = []
+    for _, input_data in data.iterrows():
+        preprocessed_data = preprocess(scaler, input_data)
+        prediction_output = model.predict([preprocessed_data])[0]
+        predictions.append(round(prediction_output, 2))
 
-    # Create a constant array with the same number of rows as the scaled input array
-    constant = np.ones((scaled_input.shape[0], 1))
+    # Create output dataframe with predicted values
+    prediction_df = pd.DataFrame({
+        'oil_rate': data['oil_rate'],
+        'depth': data['depth'],
+        'water_rate': data['water_rate'],
+        'tubing_id_1_995': data['tubing_id_1_995'],
+        'pwh': data['pwh'],
+        'prediction': predictions
+    })
 
-    # # Predict the output for the scaled input using the trained model
-    prediction_output = model.predict(np.hstack((constant, scaled_input)))
+    return prediction_df
 
-    return render_template("index.html",
-                            prediction_text = str(np.round(prediction_output[0], 2)))
-   
+def run():
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    add_selectbox = st.sidebar.selectbox(
+    "How would you like to predict?",
+    ("Online", "Batch"))
+
+    st.title("Flowing Bottom Hole Pressure Prediction App")
+
+    if add_selectbox == 'Online':
+
+        oil_rate = st.text_input('Oil Rate', value='')
+        depth = st.text_input('Depth', value='')
+        water_rate = st.text_input('Water Rate', value='')
+        tubing_id_1_995 = st.selectbox('Tubing ID 1.995', ('Yes', 'No'))
+        pwh = st.text_input('Wellhead Pressure', value='')
+
+        output=""
+
+        if st.button("Predict"):
+            # Check if any variable is missing
+            missing_vars = []
+            if not oil_rate:
+                missing_vars.append('Oil Rate')
+            if not depth:
+                missing_vars.append('Depth')
+            if not water_rate:
+                missing_vars.append('Water Rate')
+            if not pwh:
+                missing_vars.append('Wellhead Pressure')
+
+            # If any variable is missing, display an error message
+            if missing_vars:
+                st.warning("Please enter a value for the following variables: {}".format(", ".join(missing_vars)))
+
+            else:
+                # All variables are present, so proceed with prediction
+                input_dict = {'oil_rate': float(oil_rate),
+                                'depth': float(depth), 
+                                'water_rate': float(water_rate), 
+                                'tubing_id_1_995': tubing_id_1_995, 
+                                'pwh': float(pwh)}
+                output = predict_online(model, scaler, input_dict)
+                output = round(output, 2)
+                output = 'The flowing bottom-hole pressure is' + " " + str(output) + " " + 'psi'
+
+        st.success(output)
+
+    if add_selectbox == 'Batch':
+
+        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+
+        if file_upload is not None:
+            df = pd.read_csv(file_upload)
+            output_df = predict_batch(model, scaler, df)
+            st.write(output_df)
+
+            # Display the download button only if predictions are available
+            if not output_df.empty:
+                # Set the filename based on the current date and time
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                output_bytes_csv = output_df.to_csv(index=False).encode()
+                # Convert the DataFrame to an Excel file in bytes
+                # Write files to in-memory strings using BytesIO
+                output = io.BytesIO()
+                # Write the data to the Excel file using pandas
+                output_df.to_excel(output, index=False)
+                output_bytes_xlsx = output.getvalue()
+                # Send the bytes to the user as a file download
+                st.download_button(
+                    label="Download predictions as CSV file",
+                    data= output_bytes_csv,
+                    file_name=f"predictions_{timestamp}.csv",
+                    mime="text/csv",
+                )
+                st.download_button(
+                    label="Download predictions as Excel file",
+                    data=output_bytes_xlsx,
+                    file_name=f"predictions_{timestamp}.xlsx",
+                    mime="application/vnd.ms-excel",
+                )
+
+
+if __name__ == '__main__':
+    run()
